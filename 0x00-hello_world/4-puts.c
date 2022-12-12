e6f4918ca144270131a23c628ae7a0e185c6d3e9
Nkengmeche-Pride <nkengmeche@gmail.com>
2022-12-12 08:30:55 -0800

    4. Hello, puts

--- 0x00-hello_world/4-puts.c
+++ 0x00-hello_world/4-puts.c
@@ -1,4 +1,13 @@
 #include <stdio.h>
+
+/**
+ *
+ *  * Description: main - Prints out a string to stdout.
+ *
+ *   * Return: 0 if success.
+ *
+ *    */
+
 int main(void)
 
 {
@@ -6,4 +15,5 @@ int main(void)
 	puts("\"Programming is like building a multilingual puzzle");
 
 	return (0);
+
 }
