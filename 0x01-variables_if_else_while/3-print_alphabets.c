0c315042d936806633102d31b1fa1b3ed5b95715
Nkengmeche-Pride <nkengmeche@gmail.com>
2022-12-12 15:32:17 -0800

    3. alphABET

--- 0x01-variables_if_else_while/3-print_alphabets.c
+++ 0x01-variables_if_else_while/3-print_alphabets.c
@@ -6,22 +6,22 @@
  *
  *    * Return: Always 0 (Success)
  */
+
 int main(void)
-
 {
-      int ch = 'a';
-      int CH = 'A';
+int ch = 'a';
+int CH = 'A';
 
-      while (ch <= 'z')
-      {					
-	   putchar(ch);
-	      ch++;
-	      }
-       while (CH <= 'Z')
-       {
-	          putchar(CH);
-		     CH++;
+while (ch <= 'z')
+ {				
+putchar(ch);
+ch++;
+ }
+ while (CH <= 'Z')
+ {
+putchar(CH);
+CH++;
 		     }
-        putchar('\n');
-       return (0);
+putchar('\n');
+ return (0);
 }
