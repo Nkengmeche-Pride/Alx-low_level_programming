4e215affd79831d5774913499d283add6da3ca1f
Nkengmeche-Pride <nkengmeche@gmail.com>
2022-12-13 23:36:21 -0800

    14. In computer class, the first assignment was to write a program to print the first 100 Fibonacci numbers

--- 0x02-functions_nested_loops/102-fibonacci.c
+++ 0x02-functions_nested_loops/102-fibonacci.c
@@ -2,14 +2,15 @@
 
 /**
  * main - Program that prints the add of the fibonacci numbers
- *
  * Return: Always 0
  */
 
 int main(void)
+
 {
 	int c;
 	long int x, y, z;
+
 	x = 1;
 	y = 2;
 	printf("%ld, %ld", x, y);
@@ -18,7 +19,8 @@ int main(void)
 	z = x + y;
 	printf(", %ld", z);
 	x = y;
-        y = z;
+	y = z;
+	}
 	printf("\n");
 	return (0);
-	}
+}
