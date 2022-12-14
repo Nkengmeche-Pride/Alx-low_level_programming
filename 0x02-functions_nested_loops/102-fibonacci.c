d309a1a3ee12c6bb169c79c75ae239f6f81a3faa
Nkengmeche-Pride <nkengmeche@gmail.com>
2022-12-13 17:31:49 -0800

    15. Even Liber Abbaci

--- 0x02-functions_nested_loops/102-fibonacci.c
+++ 0x02-functions_nested_loops/102-fibonacci.c
@@ -7,31 +7,18 @@
  */
 
 int main(void)
-
 {
 	int c;
-
 	long int x, y, z;
-
 	x = 1;
-
 	y = 2;
-
 	printf("%ld, %ld", x, y);
-
 	for (c = 0; c < 48; c++)
-
 	{
-
 	z = x + y;
-
 	printf(", %ld", z);
-
 	x = y;
-	
-	y = z;
-																	      printf("\n");
-
+        y = z;
+	printf("\n");
 	return (0);
-
-}
+	}
