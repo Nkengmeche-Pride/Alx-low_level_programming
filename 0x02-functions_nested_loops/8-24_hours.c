ec42d896b1fdd47be8ab16e1f583e45634483838
Nkengmeche-Pride <nkengmeche@gmail.com>
2022-12-13 23:44:24 -0800

    8. I'm federal agent Jack Bauer, and today is the longest day of my life

--- 0x02-functions_nested_loops/8-24_hours.c
+++ 0x02-functions_nested_loops/8-24_hours.c
@@ -5,28 +5,30 @@
  *
  * Return: void
  */
+
 void jack_bauer(void)
 
 {
 	int a, b, c, d;
+
 	for (a = 48; a <= 50; a++)
-	{	
+	{
 	for (b = 48; b <= 57; b++)
 	{
 	for (c = 48; c <= 53; c++)
-	{	
+	{
 	for (d = 48; d <= 57; d++)
 	{
 	if (a >= 50 && b >= 52)
-	break;	
+	break;
 	_putchar(a);
-	_putchar(b); 
+	_putchar(b);
 	_putchar(58);
 	_putchar(c);
 	_putchar(d);
 	_putchar('\n');
 	}
 	}
-	}	
+	}
 	}
 }
